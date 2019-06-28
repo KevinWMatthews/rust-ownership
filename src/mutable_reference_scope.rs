06c63f5ea2ca845f0fc73b63a354c500472fd93e
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-28 09:39:11 -0700

    Create example of mutable reference scope

---
+++ src/mutable_reference_scope.rs
@@ -0,0 +1,67 @@
+fn main() {
+    example1();
+    example2();
+    example3();
+    example4();
+}
+
+fn example1() {
+    let x = 5;
+
+    let y = &x; // <-- Lifetime of reference 'y' starts
+
+    // Use the reference
+    let _val = *y; // <-- Lifetime of reference 'y' ends
+
+    // Do other stuff
+    // ...
+}
+
+fn example2() {
+    let x = 5;
+
+    let y = &x; // <-- Lifetime of reference 'y' starts
+
+    // Use the reference
+    let _val = *y;
+
+    // Do other stuff
+    // ...
+
+    // Use the reference again
+    let _val2 = *y; // <-- Lifetime of reference 'y' ends
+}
+
+fn example3() {
+    let mut x = 5;
+
+    let y = &x; // <--Lifetime of reference 'y' starts
+
+    // Use the reference
+    let _val = *y; // <-- Lifetime of reference 'y' ends
+
+    // The compiler determines that 'y' is never used again,
+    // so it considers 'y' to be out of scope.
+    // We can safely borrow mutably.
+    let z = &mut x;
+
+    // Use the reference
+    let _val = *z; // <-- Lifetime of reference 'z' ends
+}
+
+fn example4() {
+    let mut x = 5;
+
+    let y = &x; // <-- Lifetime of reference 'y' starts
+
+    // Use the reference
+    let _val = *y; // <-- Lifetime of reference 'y' ends
+
+    let z = &mut x;
+    let _val = *z; // <-- Lifetime of reference 'z' ends
+
+    let y = &x; // <-- Create a new reference 'y'
+    let _val = *y; // <-- Lifetime of new reference 'y' ends
+
+    // Do not use 'z' again! This would extend its scope.
+}
